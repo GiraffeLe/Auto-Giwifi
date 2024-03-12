9b50333a4f72632aa0e7142bd4f6affc9e0dc401
mcitem <mcitem@qq.com>
2024-03-12 21:42:08 +0800

    添加BaseURL功能，使能够适配认证服务器ip地址不同的学校

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const userInfo = {
     userMac : " "
 }
 
-var url = "http://10.53.1.3/gportal/web/login"
+var BaseURL = "http://192.168.99.120"
+var url = BaseURL+"/gportal/web/login"
 var headers = {
     //注释掉这个会提示"userName is empty"
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.5.1.4 Safari/537.36"
 }
 
-getRequest(url,headers,userInfo);
+getRequest(url,headers,userInfo,BaseURL);
