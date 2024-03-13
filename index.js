19286c3efca114a818d182db9ba9939c4e77eb24
Miya <481279665@qq.com>
2024-03-13 17:50:40 +0800

    注释掉了userMac,并优化了判断逻辑.

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const getRequest = require('./src/getRequest')
 const args = process.argv.slice(2)
 const userInfo = {
-    //请在此处填入你的账号和密码
     username : args[0],
     password : args[1],
-    userMac : " "
+    //userMac : " ",     //计划移除,网页端认证似乎用不到这个
 }
-if (args.length > 2 && args[2]) {
-    var BaseURL = "http://" + args[2]
-  } else {
-    // 未设置认证服务器ip地址时，默认地址
-    var BaseURL = "http://10.53.1.3"
-  }
-var url = BaseURL+"/gportal/web/login"
-var headers = {
+
+//如果未输入认证服务器IP,则使用默认IP
+var baseUrl = "http://10.53.1.3";
+if (args[2]!=null){
+    baseUrl = "http://" + args[2];
+}
+const headers = {
     //注释掉这个会提示"userName is empty"
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.5.1.4 Safari/537.36"
 }
 
-getRequest(url,headers,userInfo,BaseURL);
+getRequest(baseUrl,headers,userInfo);
